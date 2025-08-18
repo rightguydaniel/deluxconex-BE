573a41868b73cfcaf4b5b33e1ae962dedb09bd8e
Rightguy <rightguydaniel@gmail.com>
2025-08-18 08:43:35 +0100

    fix

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export const index = async (request: Request, response: Response) => {
   response.status(200).json({
     status: `success`,
-    message: `Welcome to Miratonrose API V2!`,
+    message: `Welcome to DeluxConex API V1`,
     error: false,
   });
 };
