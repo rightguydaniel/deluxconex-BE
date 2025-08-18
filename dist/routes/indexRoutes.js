739a9b188d3f80435407aa182a713809c9c13b28
Rightguy <rightguydaniel@gmail.com>
2025-08-18 08:13:33 +0100

    fix

--- dist/routes/indexRoutes.js
+++ dist/routes/indexRoutes.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,4 +18,11 @@ const userRoutes_1 = __importDefault(require("./userRoutes"));
 const indexRoutes = express_1.default.Router();
 indexRoutes.get("/", controllers_1.index);
 indexRoutes.use("/user", userRoutes_1.default);
+indexRoutes.get("/test", (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    response.status(200).json({
+        status: `success`,
+        message: `Testing Deployment`,
+        error: false,
+    });
+}));
 exports.default = indexRoutes;
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import { index } from "../controllers";
 import userRoutes from "./userRoutes";
+import { Request, Response } from "express";
 
 const indexRoutes = express.Router();
 indexRoutes.get("/", index);
-indexRoutes.use("/user", userRoutes)
+indexRoutes.use("/user", userRoutes);
+indexRoutes.get("/test", async (request: Request, response: Response) => {
+  response.status(200).json({
+    status: `success`,
+    message: `Testing Deployment`,
+    error: false,
+  });
+});
 
 export default indexRoutes;
