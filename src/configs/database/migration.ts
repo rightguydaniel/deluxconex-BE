7a36273a248e3a808e076afd66f1de34cbad370d
Rightguy <rightguydaniel@gmail.com>
2025-09-17 19:47:26 +0100

    fix

--- src/configs/database/migration.ts
+++ src/configs/database/migration.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    dialect: `${process.env.DB_TYPE === "postgres" ? "postgres" : "mysql"}`,
+    dialect: "mysql",
     host: `${process.env.DB_HOST}`,
     username: `${process.env.DB_USERNAME}`,
     password: `${process.env.DB_PASSWORD}`,
